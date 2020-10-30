0fe5e6930b5d27dc60f3cb4ecf0cb104eb6ef1f4
yonghan <wangyh.92@gmail.com>
2020-10-30 10:21:14 +0800

    check the url

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const withQuery = require('with-query').default
 //configure to env
 const PORT = parseInt(process.argv[2]) || parseInt(process.env.PORT) || 3000;
 const API_KEY = process.env.API_KEY
+const URL = 'https://newsapi.org/v2/top-headlines'
 
 //express instance and handlebars
 const app = express(); 
@@ -20,10 +21,33 @@ app.get('/',(req,res)=>{
     res.render('index')
 })
 
+app.get('/result', (req,res)=>{
+    res.status(200)
+    res.type('text/html')
+    res.render('result')
+})
+
+/************** Breakdown the url **************/
+//!https://newsapi.org/v2/top-headlines
+//?country=de
+//&q=trump
+//&category=business
+//&apiKey=b09895f28f3f4851906803ecf2cddd79
+
 app.post('/index',
     express.urlencoded({extended: true}), 
     (req,res,next)=>{
         console.log(req.body)
+        const url = withQuery(
+            URL,
+            {
+                q: req.body.search,
+                country: req.body.country,
+                category: req.body.category,
+                apiKey: API_KEY,
+            }
+        )
+        console.log(url)
 })
 
 //access to statics file
